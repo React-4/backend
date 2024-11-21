64d8fcf316af396daff300d0677d10d97e740cb8
ham <tomy8964@naver.com>
2024-11-21 13:11:16 +0900

    KAN-26--feat:로그인 API 추가
    :로그인 API 추가
    ApiResponse -> ApiCustomResponse 이름 변경
    ErrorResponse -> ErrorCustomResponse 이름 변경
    JwtConfig 파일 생성

--- announcement/src/main/java/org/pda/announcement/util/api/ApiCustomResponse.java
+++ announcement/src/main/java/org/pda/announcement/util/api/ApiCustomResponse.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ErrorResponse {
-    private String error;
+public class ApiCustomResponse {
+    private String message;
+    private Object data;
+
+    public ApiCustomResponse(String message) {
+        this.message = message;
+    }
 }
