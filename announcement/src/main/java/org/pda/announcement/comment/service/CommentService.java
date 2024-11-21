ac41d8570699e60a4351ed13fe4f05a1d7936d6d
ham <tomy8964@naver.com>
2024-11-21 16:32:46 +0900

    KAN-29--feat: Comment 작성 수정 삭제 API 개발
    feat: Comment 작성 수정 삭제 API 개발

--- announcement/src/main/java/org/pda/announcement/comment/service/CommentService.java
+++ announcement/src/main/java/org/pda/announcement/comment/service/CommentService.java
@@ -1,4 +1,9 @@
 package org.pda.announcement.comment.service;
 
+import org.pda.announcement.comment.dto.CommentRequest;
+
 public interface CommentService {
+    void createComment(Long announcementId, String content, String email);
+
+    void updateComment(Long commentId, CommentRequest commentRequest, String email);
 }
