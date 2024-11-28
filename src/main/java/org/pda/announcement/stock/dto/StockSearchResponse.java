953aeab1868355e0977febe87694897aee38e57f
ham <tomy8964@naver.com>
2024-11-28 09:19:54 +0900

    Update StockSearchResponse.java

--- src/main/java/org/pda/announcement/stock/dto/StockSearchResponse.java
+++ src/main/java/org/pda/announcement/stock/dto/StockSearchResponse.java
@@ -6,7 +6,7 @@ import org.pda.announcement.stock.domain.Stock;
 
 @Data
 public class StockSearchResponse {
-
+    private Long stockId;
     private Long marketCap;
     private String ticker;
     private MarketType marketType;
@@ -17,6 +17,7 @@ public class StockSearchResponse {
     // Stock 엔티티를 StockSearchResponse로 변환하는 정적 메서드 추가
     public static StockSearchResponse fromStock(Stock stock) {
         StockSearchResponse response = new StockSearchResponse();
+        response.setStockId(stock.getId());
         response.setMarketCap(stock.getMarketCap());
         response.setTicker(stock.getTicker());
         response.setMarketType(stock.getMarketType());
